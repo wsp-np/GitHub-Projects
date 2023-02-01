09e06d569b6241e2f27f10b51cd61bae6fb65349
tingtrbl <122769478+tingtrbl@users.noreply.github.com>
2023-02-01 15:36:39 +0800

    Create cash_on_hand.py

--- main.py/cash_on_hand.py
+++ main.py/cash_on_hand.py
@@ -1,34 +1,27 @@
 from pathlib import Path
-import csv
+import csv 
 
-fp = Path.cwd()/"cash-on-hand.csv"
+fp= Path.cwd()/"csv_reports"/"cash-on-hand.csv"
 
-with fp.open(mode="r", encoding="UTF-8", newline="") as file: 
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
-
-    listofaccumulatedcashonhand = []
-    listofdays = []
-    for row in reader:
-        listofaccumulatedcashonhand.append(int(row[1]))
-        listofdays.append(int(row[0]))
-
-def cashonhand():
-    day0 = listofaccumulatedcashonhand[0]
+    
+    listofaccumulatedcashonhand.append(int(row[1]))
+    listofdays.append(int(row[0]))
+ 
+def cashonhand(): 
+    day0 =listofaccumulatedcashonhand[0]
     deficit = False
     deficitdays = []
     for i in range(len(listofaccumulatedcashonhand)-1):
         difference = listofaccumulatedcashonhand[i+1] - day0
         if difference < 0:
-            deficitdays.append(f"[CASH DEFICIT] DAY: {listofdays[i+1]}, AMOUNT: USD{round(difference,1)}\n")
+            deficitdays.append(f"[CASH DEFICIT] DAY: {listofdays[i+1]}, AMOUNT USD{round(difference,1)}\n"
             deficit = True
         day0 = listofaccumulatedcashonhand[i+1]
     if deficit == False:
-        deficitdays.append(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY.\n")
+        deficitdays.append(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY. \n")
     return deficitdays
 for i in cashonhand():
-    print (i.replace("-", ""))
-
-   
-
-   
+    print(i.replace("-","")
