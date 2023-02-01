74587586f55d6d9870318e3e12ce8cf20b56e73b
smallmountain2409 <122769483+smallmountain2409@users.noreply.github.com>
2023-02-01 16:44:25 +0800

    Update cash_on_hand.py

--- main.py/cash_on_hand.py
+++ main.py/cash_on_hand.py
@@ -21,17 +21,17 @@ def cashonhand():
     """
     Calculate the difference in cash on hand. This function does not require any parameters. 
     """
-    day0 =listofaccumulatedcashonhand[0] # c 
+    day0 =listofaccumulatedcashonhand[0] # create a variable to check for the list of accumulated cash on hand 
     deficit = False 
-    deficitdays = []
-    for i in range(len(listofaccumulatedcashonhand)-1): # loop through length of accumulated cash on hand to determine if there is a difference if there is profit deficit
-        difference = listofaccumulatedcashonhand[i+1] - day0
-        if difference < 0:
+    deficitdays = [] 
+    for i in range(len(listofaccumulatedcashonhand)-1): # loop through length of accumulated cash on hand to determine if there is a differenc in profit deficit
+        difference = listofaccumulatedcashonhand[i+1] - day0 
+        if difference < 0: # check if there is a difference in the profit deficit 
             deficitdays.append(f"[CASH DEFICIT] DAY: {listofdays[i+1]}, AMOUNT USD{round(difference,1)}\n"
-            deficit = True
+            deficit = True 
         day0 = listofaccumulatedcashonhand[i+1]
-    if deficit == False:
+    if deficit == False: # 
         deficitdays.append(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY. \n")
-    return deficitdays
-for i in cashonhand():
-    print(i.replace("-","")
+    return deficitdays # return the number of deficit days 
+for i in cashonhand(): 
+    print(i.replace("-","") # print the function, replace the negative sign and execute the file 
