b67a8587d10b36c996e267fc9c611257ccfc00cf
wsp-np <122768345+wsp-np@users.noreply.github.com>
2023-02-01 15:27:21 +0800

    Update overheads.py

--- main.py/overheads.py
+++ main.py/overheads.py
@@ -1,8 +1,32 @@
 from pathlib import Path
 import csv 
 
-fp= Path.cwd()/"csv_reports"/"cash-on-hand.csv"
+fp= Path.cwd()/"csv_reports"/"overheads-day-90.csv"
 
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
+
+    category = []
+    value = []
+
+    for x in reader:
+        category.append(x[0])
+        value.append(float(x[1]))
+    
+def overhead():
+    """
+    This finds the highest overhead category and its value when executed. 
+    This function does not need parameter.
+    """
+
+    high = []
+    for highest in value:
+        high.append(max(value))
+        break
+
+    idx = high.index(max(value))
+
+    return f"[HIGHEST OVERHEADS] {(category[idx]).upper()}: USD {round(high[0],1)}%"
+
+print(overhead())
