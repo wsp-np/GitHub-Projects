49ce2bf1690888dfe7a9a2a1f60b76dbb9cf411b
smallmountain2409 <122769483+smallmountain2409@users.noreply.github.com>
2023-02-01 15:53:15 +0800

    Update cash_on_hand.py

--- main.py/cash_on_hand.py
+++ main.py/cash_on_hand.py
@@ -1,20 +1,24 @@
 from pathlib import Path
 import csv 
 
+# create a file to csv file  
 fp= Path.cwd()/"csv_reports"/"cash-on-hand.csv"
 
+# read the csv file to append cash on hand from the csv file 
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
     
+    # append accumulated cash on hand and days as a list back to each empty list 
     listofaccumulatedcashonhand.append(int(row[1]))
     listofdays.append(int(row[0]))
  
+# create a function to calculate 
 def cashonhand(): 
-    day0 =listofaccumulatedcashonhand[0]
-    deficit = False
+    day0 =listofaccumulatedcashonhand[0] # check for the accumulated cash on hand 
+    deficit = False 
     deficitdays = []
-    for i in range(len(listofaccumulatedcashonhand)-1):
+    for i in range(len(listofaccumulatedcashonhand)-1): # create a loop, 
         difference = listofaccumulatedcashonhand[i+1] - day0
         if difference < 0:
             deficitdays.append(f"[CASH DEFICIT] DAY: {listofdays[i+1]}, AMOUNT USD{round(difference,1)}\n"
